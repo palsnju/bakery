938ea73fe3fc359c6781142a0f85cd8a2ee10f2c
Sanjay <palsnju@gmail.com>
2018-11-15 00:52:14 +0530

    1st

---
+++ BakerySystemManagment/js1.js
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+  function readURL(input) {
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+
+                reader.onload = function (e) {
+                    $('#blah')
+                        .attr('src', e.target.result);
+                };
+
+        consol.log("hiu");        reader.readAsDataURL(input.files[0]);
+            }
+        }
+  
+  
+  
+  function previewFile(){
+      var preview = document.querySelector('img'); //selects the query named img
+      var file    = document.querySelector('input[type=file]').files[0]; //sames as here
+      var reader  = new FileReader();
+
+      reader.onloadend = function () {
+          preview.src = reader.result;
+      }
+
+      if (file) {
+          reader.readAsDataURL(file); //reads the data as a URL
+      } else {
+          preview.src = "";
+      }
+ }
+
+ previewFile(); 
---
+++ BakerySystemManagment/src/main/java/com/ecommerce/BakerySystemManagment/BakerySystemManagmentApplication.java
@@ -0,0 +1,12 @@
+package com.ecommerce.BakerySystemManagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakerySystemManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BakerySystemManagmentApplication.class, args);
+	}
+}
